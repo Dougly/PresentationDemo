8fd8015eb3c40dd7a242f065cd1e4f790ddd900d
Dougly <dgalante11@gmail.com>
2016-10-30 18:13:44 -0400

    completed demo

--- DemoForSlider/DemoForSlider/ViewController.swift
+++ DemoForSlider/DemoForSlider/ViewController.swift
@@ -11,38 +11,23 @@ import CircularSlider
 
 class ViewController: UIViewController, CircularSliderDelegate {
     
-    @IBOutlet weak var mySlider: CircularSlider!
-    @IBOutlet weak var yellowBackground: UIView!
+    @IBOutlet weak var circleSlider: CircularSlider!
     @IBOutlet weak var redBackground: UIView!
     
-    @IBAction func updateBackground(_ sender: AnyObject) {
-        self.redBackground.alpha = CGFloat(mySlider.value)
-
-    }
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        redBackground.backgroundColor = UIColor.red
-        redBackground.alpha = 0
-        mySlider.backgroundColor = UIColor(hue: 1, saturation: 1, brightness: 1, alpha: 0)
-        yellowBackground.backgroundColor = UIColor.yellow
-        mySlider.delegate = self
-        print(mySlider.value)
+        self.redBackground.alpha = 0
+        circleSlider.delegate = self
     }
     
-    func updateColor () {
-    
-    
-    }
-
-    func circularSlider(_ circularSlider: CircularSlider, didEndEditing textfield: UITextField) {
-        print("did end editing")
+    func circularSlider(_ circularSlider: CircularSlider, valueForValue value: Float) -> Float {
+        self.redBackground.alpha = CGFloat(circleSlider.value)
+        return value
     }
     
-    func circularSlider(_ circularSlider: CircularSlider, didBeginEditing textfield: UITextField) {
-        print("did begin editing")
+    @IBAction func movedSlider(_ sender: UISlider) {
+        self.redBackground.alpha = CGFloat(sender.value)
     }
-
-
+    
 }
 
